c8c8aacec3ab86aceaa9053cbdd622712f51a60b
ali <ali.shiekh16@outlook.com>
2022-11-24 08:57:00 +0500

    Increase chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), glsl()],
-	base: '/r3f-water-drops/'
-})
+  plugins: [react(), glsl()],
+  base: '/r3f-water-drops/',
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+});
